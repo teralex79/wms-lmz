f9752b28206283a80c5b2c6b2fcdaa04289ff04c
Aleksei Terentev <teralex79@gmail.com>
2019-11-15 11:48:20 +0300

    Added new hostname for TT

--- Top/wms_cfg.tcl
+++ Top/wms_cfg.tcl
@@ -120,12 +120,14 @@ set adrDev 1
 
 set hn [info hostname]
 
-set wms(tt_adr_list) {localhost 192.168.0.101 192.168.0.102}
+set wms(tt_adr_list) {localhost 192.168.0.101 192.168.0.102 192.168.0.103}
 
 if {$hn == "td16003"} {
   set wms(adr_tt) "192.168.0.101"
 } elseif {$hn == "td15003"} {
   set wms(adr_tt) "192.168.0.102"
+} elseif {$hn == "tn12001"} {
+  set wms(adr_tt) "192.168.0.101"
 } else {
   set wms(adr_tt) "localhost"
 }
